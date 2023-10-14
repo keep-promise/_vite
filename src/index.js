cd39de9bc99c95a2abe987a90b68b86080f71bac
keep_promise <1981672801@qq.com>
2023-10-14 09:34:12 +0800

    提交代码

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const ele = document.createElement('div');
+ele.innerHTML = 'index.js';
+console.log('hello index.js');
+document.body.appendChild(ele);
--- vite.js
+++ vite.js
@@ -1,8 +1,28 @@
 const koa = require('koa');
+const fs = require('fs');
+const path = require('path');
 const app = new koa();
 
 app.use(async ctx => {
-  ctx.body = 'vite';
+  const { url } = ctx.request;
+  // html处理
+  if (url == '/') {
+    ctx.type = 'text/html';
+    const _path = path.join(__dirname, './index.html');
+    const html = fs.readFileSync(_path, 'utf-8');
+    ctx.body = html;
+    return;
+  }
+
+  // js文件处理
+  if(url.endsWith('.js')) {
+    ctx.type = 'application/javascript';
+    const _path = path.join(__dirname, url);
+    const jscontent = fs.readFileSync(_path, 'utf-8');
+    ctx.body = jscontent;
+    return;
+  }
+
 });
 
 app.listen(3000, () => {
