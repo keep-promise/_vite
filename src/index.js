060d8cff33e707829428aa742fa34ff862a7bbe0
keep_promise <1981672801@qq.com>
2023-10-14 10:30:49 +0800

    提交代码

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-const ele = document.createElement('div');
-ele.innerHTML = 'index.js';
-console.log('hello index.js');
-document.body.appendChild(ele);
+// const ele = document.createElement('div');
+// ele.innerHTML = 'index.js';
+// console.log('hello index.js');
+// document.body.appendChild(ele);
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+console.log('React', React)
+console.log('ReactDom', ReactDom)
+
+function App() {
+  return React.createElement('div', {
+    className: 'aaa',
+    children: 'App'
+  });
+};
+
+ReactDom.render(App(), document.body)
+
+
+
--- vite.js
+++ vite.js
@@ -18,12 +18,36 @@ app.use(async ctx => {
   if(url.endsWith('.js')) {
     ctx.type = 'application/javascript';
     const _path = path.join(__dirname, url);
-    const jscontent = fs.readFileSync(_path, 'utf-8');
-    ctx.body = jscontent;
+    const content = fs.readFileSync(_path, 'utf-8');
+    ctx.body = rewriteImportPath(content);
     return;
   }
 
+  // 第三方模块加载
+  if(url.startsWith('/@modules')) {
+    const moduleName = url.replace('/@modules', '');
+    const prefix = path.join(__dirname, './node_modules', moduleName);
+    const module = require(prefix +'/package.json').module;
+    const filePath = path.join(prefix, module);
+    const content = fs.readFileSync(filePath, 'utf-8');
+    ctx.type = 'application/javascript';
+    ctx.body = rewriteImportPath(content);
+  }
+
 });
+
+// import xxx from react --》import xxx from /@module/react
+// es module浏览器不支持第三方模块加载，替换入口文件中第三方中的路径成绝对路径
+function rewriteImportPath(content) {
+  return content.replace(/ from ['"](.*)['"]/g, (s1, s2) => {
+    // 普通模块
+    if (s2.startsWith('.') || s2.startsWith('./') || s2.startsWith('../')) {
+      return s1; 
+    } else { // 第三方模块
+      return ` from '/@modules/${s2}'`;
+    }
+  })
+}
 
 app.listen(3000, () => {
   console.log('serve runing at localhost:3000');
